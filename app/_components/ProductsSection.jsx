c330c6ec464ff98a7f25a4f6a21f4f00d2f63249
Andrew Magdy <89481184+Andrew-Magdy-Rizk@users.noreply.github.com>
2025-01-13 13:44:40 +0200

    update productSection

--- app/_components/ProductsSection.jsx
+++ app/_components/ProductsSection.jsx
@@ -1,16 +1,16 @@
-import React, { Suspense } from 'react'
-import ProductList from './ProductList'
-import Loading from '@/app/loading'
+import React, { Suspense } from "react";
+import ProductList from "./ProductList";
+import Loading from "../loading";
 
 function ProductsSection() {
   return (
-    <div className='container mx-auto px-5 lg:px-10'>
-      <h1 className='text-3xl font-bold dark:text-white'>Products Section</h1>
-      <Suspense fallback={<Loading/>}>
-      <ProductList/>
+    <div className="container mx-auto px-5 lg:px-10">
+      <h1 className="text-3xl font-bold dark:text-white">Products Section</h1>
+      <Suspense fallback={<Loading />}>
+        <ProductList />
       </Suspense>
     </div>
-  )
+  );
 }
 
-export default ProductsSection
+export default ProductsSection;
