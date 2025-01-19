4e525e48bc4ed5b4ae97de8d3a1b36192bf6c7ab
Andrew Magdy <89481184+Andrew-Magdy-Rizk@users.noreply.github.com>
2025-01-19 13:40:58 +0200

    update header admin

--- app/(users)/admin/_components/SlideMenu.jsx
+++ app/(users)/admin/_components/SlideMenu.jsx
@@ -283,8 +283,37 @@ function SlideMenu() {
             </li>
           </ul>
         </div>
-        <div className="mx-auto py-6">
+        <div className="mx-auto py-6  sticky inset-x-0 bottom-0">
           <ThemesMode />
+        </div>
+        <div className="block md:hidden sticky inset-x-0 bottom-0 border-t border-gray-100 dark:border-gray-500 p-2">
+          <form action="#">
+            <Link
+              href="/login"
+              onClick={() => dispatch(logoutReducer())}
+              type="submit"
+              className="group relative flex w-full justify-center rounded-lg px-2 py-1.5 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700 dark:text-white dark:hover:bg-gray-700"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-5 opacity-75"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                />
+              </svg>
+
+              <span className="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible">
+                Logout
+              </span>
+            </Link>
+          </form>
         </div>
       </div>
     </aside>
