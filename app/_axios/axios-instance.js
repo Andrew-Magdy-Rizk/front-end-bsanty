4dc0d16c2a967422036d4d277bc4b18d50a73921
Andrew Magdy <89481184+Andrew-Magdy-Rizk@users.noreply.github.com>
2025-01-26 03:56:25 +0200

    change server

--- app/_axios/axios-instance.js
+++ app/_axios/axios-instance.js
@@ -1,6 +1,6 @@
 import { default as axios } from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://server-bsnty.vercel.app/api/v1",
   timeout: 30000,
 });
 
