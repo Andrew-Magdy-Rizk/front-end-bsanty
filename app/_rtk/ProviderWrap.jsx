5623c24562a392aeccf7d925061ee4921947b3c1
Andrew Magdy <89481184+Andrew-Magdy-Rizk@users.noreply.github.com>
2025-02-14 20:03:09 +0200

    bug import

--- app/_rtk/ProviderWrap.jsx
+++ app/_rtk/ProviderWrap.jsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import { useEffect } from "react";
 import Cookies from "js-cookie";
+import { loginReducer } from "./slices/authReducers";
 
 function ProviderWrap({ children }) {
   const handelUser = () => {
